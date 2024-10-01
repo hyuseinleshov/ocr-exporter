c1b46eaf2dd2c5a9bc0a8cd439196cbd2c938a63
hyuseinlesho <hiuseinleshogithub@gmail.com>
2024-10-01 16:05:29 +0300

    Initial commit

---
+++ src/main/java/com/hyuseinlesho/ocrexporterapi/OcrExporterApiApplication.java
@@ -0,0 +1,13 @@
+package com.hyuseinlesho.ocrexporterapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OcrExporterApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OcrExporterApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hyuseinlesho/ocrexporterapi/OcrExporterApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyuseinlesho.ocrexporterapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OcrExporterApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
