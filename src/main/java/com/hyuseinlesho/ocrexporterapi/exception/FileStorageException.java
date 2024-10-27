e78e936e59676c3ada72ca5781ea019ed1164eb0
hyuseinleshov <hyusein.leshov@gmail.com>
2024-10-27 11:55:39 +0200

    feat(error-handling): add custom exception for file saving errors

---
+++ src/main/java/com/hyuseinlesho/ocrexporterapi/exception/FileStorageException.java
@@ -0,0 +1,7 @@
+package com.hyuseinlesho.ocrexporterapi.exception;
+
+public class FileStorageException extends RuntimeException {
+    public FileStorageException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
